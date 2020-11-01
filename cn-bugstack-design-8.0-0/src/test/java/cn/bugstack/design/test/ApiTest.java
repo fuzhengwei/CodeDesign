270cbfb90d61029d609bd8611f96b2a64fd760e0
fuzhengwei <184172133@qq.com>
2020-11-01 09:43:05 +0800

    第 8 章 单例模式

---
+++ cn-bugstack-design-8.0-0/src/test/java/cn/bugstack/design/test/ApiTest.java
@@ -0,0 +1,13 @@
+package cn.bugstack.design.test;
+
+import cn.bugstack.design.Singleton_07;
+import org.junit.Test;
+
+public class ApiTest {
+
+    @Test
+    public void test() {
+        Singleton_07.INSTANCE.test();
+    }
+
+}

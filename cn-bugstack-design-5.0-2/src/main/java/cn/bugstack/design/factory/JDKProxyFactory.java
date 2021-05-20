dfc89e5d4a32cac365d395a809f0e67cb30f8062
小傅哥 <fuzhengwei@users.noreply.github.com>
2021-05-20 10:42:26 +0800

    抽象工厂 Class<T> cacheClazz

--- cn-bugstack-design-5.0-2/src/main/java/cn/bugstack/design/factory/JDKProxyFactory.java
+++ cn-bugstack-design-5.0-2/src/main/java/cn/bugstack/design/factory/JDKProxyFactory.java
@@ -7,7 +7,7 @@ import java.lang.reflect.Proxy;
 
 public class JDKProxyFactory {
 
-    public static <T> T getProxy(Class cacheClazz, Class<? extends ICacheAdapter> cacheAdapter) throws Exception {
+    public static <T> T getProxy(Class<T> cacheClazz, Class<? extends ICacheAdapter> cacheAdapter) throws Exception {
         InvocationHandler handler = new JDKInvocationHandler(cacheAdapter.newInstance());
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         return (T) Proxy.newProxyInstance(classLoader, new Class[]{cacheClazz}, handler);
